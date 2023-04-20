b48460752fe86aa6fc230f70a75d899d3da5a07d
israferna <israferna@penguin>
2023-04-20 21:09:20 +0200

    Acabamos el video 8

---
+++ Proyecto1/views.py
@@ -0,0 +1,56 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import get_template
+import datetime
+
+class Persona(object):
+    def __init__(self,nombre,apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
+def saludo(request): # primera vista
+
+    nombre="Juan"
+    apellido="Perez"
+    p1=Persona(nombre,apellido)
+    fecha_actual=datetime.datetime.now()
+
+    #doc_externo=open("/home/israferna/Documents/VisualStudio/ProyectosDjango/Proyecto1/Proyecto1/plantillas/miplantilla.html")
+    #plt=Template(doc_externo.read())
+    #doc_externo.close()
+    doc_externo=get_template('miplantilla.html')
+
+
+    temario=["Modelos",
+             "Plantillas",
+             "Comentarios",
+             "Contextos"]
+
+    ctx=Context({
+        "nombre_persona":p1.nombre,
+        "apellido_persona":p1.apellido, 
+        "fecha":fecha_actual,
+        "temas":temario})
+
+    documento=doc_externo.render(ctx)
+
+    return HttpResponse(documento)
+
+def despedida(request):
+
+    return HttpResponse("Nos vemos pronto")
+
+
+def dameFecha(request):
+    fecha_actual=datetime.datetime.now()
+    documento="""
+    <html>
+        <body>
+        <h2>
+            Fecha y hora actuales %s
+        </h2>
+        </body>
+    </html>
+    """ % fecha_actual
+
+    return HttpResponse(documento)
